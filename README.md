47d7d5546c3c4df4d62ad27ef8982d61e38ad916
adamantris <152009291+adamantris@users.noreply.github.com>
2025-11-28 19:10:59 +0100

    Add: Fish size related fixes

--- README.md
+++ README.md
@@ -13,5 +13,6 @@ This project aims to fix all remaining game issues and incomplete/unreleased con
 - Users blocked on Steam are now also blocked in-game
 - Lobbies with a player cap higher than 12 are no longer hidden from the lobby browser list
 - Lobbies with more than 12 players are not marked as `Modded`
+- Fish with unusual sizes now have their size prefix and worth multiplier applied correctly
 
 </details>
--- scriptmod/Mod.cs
+++ scriptmod/Mod.cs
@@ -107,6 +107,73 @@ public class Mod : IMod
 				)
 				.Build()
 		);
+
+		mi.RegisterScriptMod(
+			new TransformationRuleScriptModBuilder()
+				.ForMod(mi)
+				.Named("Fish size fixes")
+				.Patching("res://Scenes/Singletons/playerdata.gdc")
+				.AddRule(
+					new TransformationRuleBuilder()
+						.Named("Size prefix fix")
+						.Do(Operation.Append)
+						.Matching(
+							TransformationPatternFactory.CreateGdSnippetPattern(
+								"""
+								var prefix = ""
+
+								for p in size_prefix.keys():
+								"""
+							)
+						)
+						.With(
+							"""
+
+							if calc < 0.1:
+								prefix = size_prefix[0.1]
+								break
+
+							elif calc > 3.25:
+								prefix = size_prefix[3.25]
+
+							""",
+							2
+						)
+						.ExpectTimes(2)
+				)
+				.AddRule(
+					new TransformationRuleBuilder()
+					.Named("Size/worth mult fix")
+					.Do(Operation.Append)
+					.Matching(
+						TransformationPatternFactory.CreateGdSnippetPattern(
+							"""
+							var average = Globals.item_data[data["id"]]["file"].average_size
+							var calc = data["size"] / average
+							var mult = 1.0
+
+							for p in size_prefix.keys():
+							"""
+						)
+					)
+					.With(
+						"""
+
+						if calc < 0.1:
+							mult = size_prefix[0.1]
+							break
+
+						elif calc > 3.0:
+							mult = size_prefix[3.0]
+							break
+
+
+						""",
+						2
+					)
+				)
+				.Build()
+		);
 		// }
 	}
 
